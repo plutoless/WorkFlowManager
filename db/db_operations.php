dae5fcd01ebcbf31f131d8ac0e04c2d61178fa68
plutoless <qianze.zhang@gmail.com>
2014-01-28 17:00:11 +0800

    prevent initial device id check

--- db/db_operations.php
+++ db/db_operations.php
@@ -9,11 +9,32 @@ include 'db.php';
         }
         
         public function login($user, $pwd, $did){
-            $query = sprintf("SELECT * FROM user WHERE username='%s' AND passwd='%s' AND did='%s'",
-                                mysql_real_escape_string($user), mysql_real_escape_string($pwd), mysql_real_escape_string($did));
+            $query = sprintf("SELECT * FROM user WHERE username='%s' AND passwd='%s'",
+                                mysql_real_escape_string($user), mysql_real_escape_string($pwd));
             $result = mysql_query($query) or die(mysql_error());
             $row = mysql_fetch_assoc($result);
-            return array('data'=>$row, 'valid'=>$row?true:false);
+            if($row){
+                //check if did exists
+                if(is_null($row['did']) || $row['did'] == ""){
+                    //if not, we set the passed $did to sql
+                    $query = sprintf("UPDATE user SET did='%s' WHERE uid=%d",
+                                mysql_real_escape_string($did), $row['uid']);
+                    $result = mysql_query($query) or die(mysql_errno());
+                    $row['did'] = $did;
+                    return array('data'=>$row, 'valid'=>true);
+                } else {
+                    //else, see if the passed value matches
+                    if($did == $row['did']){
+                        return array('data'=>$row, 'valid'=>true); 
+                    } else {
+                        //not match, return failure
+                        return array('data'=>false, 'valid'=>false); 
+                    }
+                }
+            } else {
+                //passwd not match with user
+                return array('data'=>$row, 'valid'=>false);
+            }
         }
         
         public function submitMessage($uid, $text, $pos, $attachment){
